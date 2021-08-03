9d0b4755009e0c02c8b43e50d683fca424822850
zf49 <zwan686@aucklanduni.ac.nz>
2021-08-03 20:04:21 +0800

    11

--- mybatis-08-2/src/test/java/mytestFile.java
+++ mybatis-08-2/src/test/java/mytestFile.java
@@ -2,24 +2,30 @@ import com.kuang.dao.BlogMapper;
 import com.kuang.pojo.Blog;
 import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.List;
 
 public class mytestFile {
-    public static void main(String[] args) {
+
+
+
+
+
+
+    @Test
+    public void testIf(){
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
         HashMap map = new HashMap<>();
-            map.put("title","Mybatis SO easy");
-            map.put("author","狂神说");
+        map.put("title","Mybatis SO easy");
+        map.put("author","狂神说");
         List<Blog> blogs =  mapper.queryBlogIf(map);
         for (Blog blog : blogs) {
             System.out.println(blog);
         }
         sqlSession.close();
     }
-
-
 
 }
